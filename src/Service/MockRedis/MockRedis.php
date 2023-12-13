6ea6d1214c3b447ab0f46432cfa7d86a3b827707
Roberlay León Piñero <roberlayl@gmail.com>
2023-12-13 15:48:15 -0500

    feat: Update "MockRedis" service

--- src/Service/MockRedis/MockRedis.php
+++ src/Service/MockRedis/MockRedis.php
@@ -29,6 +29,11 @@ class MockRedis extends Redis
         $this->filesDir = $parameterBag->get('kernel.project_dir') . '/vendor/experteam/api-testing-bundle/data/redis-mock';
     }
 
+    public function get(string $key): mixed
+    {
+        return $this->getValue(__FUNCTION__, $key);
+    }
+
     /**
      * @param string $function
      * @param string $key
@@ -52,23 +57,18 @@ class MockRedis extends Redis
         return file_get_contents($filePath);
     }
 
-    public function get($key)
-    {
-        return $this->getValue(__FUNCTION__, $key);
-    }
-
-    public function hGet($key, $hashKey)
+    public function hGet(string $key, string $member): mixed
     {
         return $this->getValue(__FUNCTION__, $key, $hashKey);
     }
 
-    public function hGetAll($key)
+    public function hGetAll(string $key): Redis|array|false
     {
         $value = $this->getValue(__FUNCTION__, $key);
         return (($value === false) ? [] : json_decode($value, true));
     }
 
-    public function incr($key)
+    public function incr(string $key, int $by = 1): Redis|int|false
     {
         $dsn = new RedisDsn($_ENV['REDIS_URL'] ?? '');
         parent::connect($dsn->getHost(), $dsn->getPort(), 5, $dsn->getPersistentId(), 5, 5);
@@ -80,25 +80,25 @@ class MockRedis extends Redis
         $keys = json_decode($this->getValue(__FUNCTION__, __FUNCTION__), true)[__FUNCTION__] ?? [];
 
         return array_filter($keys, function ($k) use ($pattern) {
-            $pattern = preg_replace_callback('/([^*])/', function($m) {
+            $pattern = preg_replace_callback('/([^*])/', function ($m) {
                 return preg_quote($m[0], '/');
             }, $pattern);
             $pattern = str_replace('*', '.*', $pattern);
-            return (bool) preg_match('/^' . $pattern . '$/i', $k);
+            return (bool)preg_match('/^' . $pattern . '$/i', $k);
         });
     }
 
-    public function set($key, $value, $timeout = null)
+    public function set(string $key, mixed $value, mixed $options = null): Redis|string|bool
     {
         //Do nothing
     }
 
-    public function hSet($key, $hashKey, $value)
+    public function hSet(string $key, string $member, mixed $value): Redis|int|false
     {
         //Do nothing
     }
 
-    public function expire($key, $ttl)
+    public function expire(string $key, int $timeout, ?string $mode = null): Redis|bool
     {
         //Do nothing
     }
