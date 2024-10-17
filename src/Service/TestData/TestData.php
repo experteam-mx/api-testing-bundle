f470a7647d13613d1edae82d3a6736f296d7d54d
Roberlay León Piñero <roberlayl@gmail.com>
2024-10-16 19:06:08 -0500

    feat: Improve test data service

--- src/Service/TestData/TestData.php
+++ src/Service/TestData/TestData.php
@@ -4,37 +4,23 @@ namespace Experteam\ApiTestingBundle\Service\TestData;
 
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class TestData implements TestDataInterface
 {
-    /**
-     * @var string
-     */
     private string $filesDir;
-
-    /**
-     * @var Filesystem
-     */
     private Filesystem $filesystem;
 
-    /**
-     * @param ParameterBagInterface $parameterBag
-     */
     public function __construct(ParameterBagInterface $parameterBag)
     {
         $this->filesystem = new Filesystem();
         $this->filesDir = $parameterBag->get('kernel.project_dir') . '/tests/Data/';
     }
 
-    /**
-     * @param string $class
-     * @param string $function
-     * @param string $key
-     * @return array|null
-     */
     public function getValue(string $class, string $function, string $key): ?array
     {
-        $filePath = $this->filesDir . str_replace(['\\', 'App/Tests/'], ['/', ''], $class) . '/' . ucfirst($function) . "/$key.json";
+        $filePath = "{$this->getTestPath($class, $function)}/$key.json";
 
         if (!$this->filesystem->exists($filePath)) {
             return null;
@@ -47,5 +33,34 @@ class TestData implements TestDataInterface
         }
 
         return json_decode($fileContents, true);
+    }
+
+    private function getTestPath(string $class, string $function): string
+    {
+        return $this->filesDir . str_replace(['\\', 'App/Tests/'], ['/', ''], $class) . '/' . ucfirst($function);
+    }
+
+    public function getFiles(string $class, string $function): ?array
+    {
+        $filesPath = "{$this->getTestPath($class, $function)}/files";
+
+        if (!$this->filesystem->exists($filesPath)) {
+            return null;
+        }
+
+        $finder = new Finder();
+        $finder->files()->in($filesPath);
+
+        if (!$finder->hasResults()) {
+            return null;
+        }
+
+        $files = [];
+
+        foreach ($finder as $file) {
+            $files[$file->getFilenameWithoutExtension()] = new UploadedFile($file->getRealPath(), $file->getFilename(), mime_content_type($file->getRealPath()));
+        }
+
+        return $files;
     }
 }
--- src/Service/TestData/TestDataInterface.php
+++ src/Service/TestData/TestDataInterface.php
@@ -4,11 +4,7 @@ namespace Experteam\ApiTestingBundle\Service\TestData;
 
 interface TestDataInterface
 {
-    /**
-     * @param string $class
-     * @param string $function
-     * @param string $key
-     * @return array|null
-     */
     public function getValue(string $class, string $function, string $key): ?array;
+
+    public function getFiles(string $class, string $function): ?array;
 }
